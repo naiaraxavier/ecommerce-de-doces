2954fdf43ee6b5558295ba048cebbafe89bc92fd
Naiara Martins <naiaraxf@gmail.com>
2024-03-18 23:52:12 -0300

    feat:page home and css

--- app/frontend/src/pages/Home.jsx
+++ app/frontend/src/pages/Home.jsx
@@ -1,13 +1,17 @@
+import Carousel from "../components/Carousel";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import About from "../components/About";
 
 
 function Home() {
   return (
-    <>
+    <main>
       <Header />
       <Navbar />
-    </>
+      <Carousel />
+      <About />
+    </main>
   )
 }
 
--- app/frontend/src/pages/Products.jsx
+++ app/frontend/src/pages/Products.jsx
@@ -1,7 +1,13 @@
+import Header from "../components/Header"
+import Navbar from "../components/Navbar"
 
 function Products() {
   return (
-    <div>Products</div>
+    <>
+    <Header />
+    <Navbar />
+    <h2>Produtos</h2>
+    </>
   )
 }
 
